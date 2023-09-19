55820834f1e4b18a84f5865d921bb51739d80144
Luigi Bertelli <berteluigi@gmail.com>
2023-09-19 16:23:26 -0300

    Criando Aplicativo Mobile do projeto

---
+++ Sprint1MobileProject/Sprint1MobileProject/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Configuration;
+using Sprint1MobileProject.Utils;
+
+namespace Sprint1MobileProject
+{
+    public partial class MainPage : ContentPage
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnBtnOrderClicked(object sender, EventArgs e)
+        {
+            var uri = @"";
+            var fixDict = new Dictionary<int, string>()
+            {
+                { 35, "D" },
+                { 49, "CLIENT1" },
+                { 56, "EXECUTOR" },
+                { 1, "ACC1" },
+                { 34, "3" },
+                { 52, DateTime.UtcNow.ToString() },
+                { 11, "1288152766"},
+                { 21, "1" },
+                { 55, FixUtil.GenerateRandomTag55() },
+                { 54, "1" },
+                { 60, DateTime.UtcNow.ToString() },
+                { 40, "2" },
+                { 44, FixUtil.GenerateRandomTag44().ToString() },
+                { 38, "1000" }
+            };
+
+            var result = await client.PostAsJsonAsync<object>(uri, new { FIX = FixUtil.GenerateFIX(fixDict)});
+        }
+    }
+}
---
+++ Sprint1MobileProject/Sprint1MobileProject/Utils/FixUtil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Text;
+
+namespace Sprint1MobileProject.Utils
+{
+    public static class FixUtil
+    {
+        public static string GenerateRandomTag55(int len = 20)
+        {
+            var charArray = new char[len];
+            var rand = new Random();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                charArray[i] = Convert.ToChar(rand.Next(94) + 32);
+            }
+            
+
+            return charArray.ToString();
+        }
+
+        public static decimal GenerateRandomTag44(decimal upperBound = 100L)
+        {
+            var rand = new Random();
+
+            return rand.Next((int) Math.Round(upperBound, 2) * 100) / 100;
+        }
+
+        public static string GenerateFIX(Dictionary<int, string> dict)
+        {
+            StringBuilder fix = new StringBuilder();
+            const string version = "FIX.4.4";
+
+            foreach(var kvp in dict ) 
+            {
+                fix.Append($"{kvp.Key}={kvp.Value}\u0001");
+            }
+
+            fix.Insert(0, fix.Length.ToString());
+            fix.Insert(0, version);
+
+            var tag10 = $"10={GenerateTag10(fix.ToString())}\u0001";
+
+            return fix.ToString() + tag10;
+        }
+
+        public static string GenerateTag10(string fix)
+        {
+            var sum = 0;
+            foreach( var c in fix ) 
+            {
+                sum += c;
+            }
+
+            var mod = sum % 256;
+
+            return mod.ToString().PadLeft(3, '0');
+        }
+    }
+}
